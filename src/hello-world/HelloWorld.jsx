a764a6f56de636f7591c1863627d726294aec0e4
sevinadewi <selviepurnamadewi@gmail.com>
2025-03-08 13:43:35 +0700

    latihan

---
+++ src/hello-world/HelloWorld.jsx
@@ -0,0 +1,21 @@
+export default function HelloWorld(){
+    return (
+        <div>
+            <HeaderHelloworld/>
+            <ParagrafHelloworld/>
+            
+        </div>
+    )
+}
+
+function HeaderHelloworld(){
+    return (
+        <h1>Helloworld</h1>
+    )
+}
+
+function ParagrafHelloworld(){
+    return (
+        <p>belajar gess</p>
+    )
+}
